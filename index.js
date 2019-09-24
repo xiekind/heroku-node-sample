3357bd5f6df315e0b99ef22899813e50964ccbbb
Gracejean <jennilyn.mendoza@student.passerellesnumeriques.org>
2019-09-24 16:14:32 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var port = process.env.PORT || 3000
+
+// Serving HTML
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(port, function () {
+    console.log('listening on *:' + port);
+});
